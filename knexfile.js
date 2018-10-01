938e973144b0e3e50778d788375c9066a084adb4
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-10-01 11:12:39 -0500

    hope to deploy

--- knexfile.js
+++ knexfile.js
@@ -9,13 +9,8 @@ module.exports = {
 
 
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  
-
+  production: {
+    client: 'pg',
+    connection:'postgres://thzzntrlszolch:70c71b7b09a783346d4e2ea7c950e78ec7ebc977994ec31a83bf854fbffba8a7@ec2-23-23-253-106.compute-1.amazonaws.com:5432/df7kpk6di0jkmd'
+  }
 };
